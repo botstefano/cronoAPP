a29f3919019d84bf30e349639cec310d20c23f2d
botstefano <t1023300721@unitru.edu.pe>
2026-07-08 15:25:13 -0500

    Corregir error de sintaxis en dashboard

--- flutter_app/lib/screens/dashboard_screen.dart
+++ flutter_app/lib/screens/dashboard_screen.dart
@@ -282,7 +282,6 @@ class _DashboardScreenState extends State<DashboardScreen> with WidgetsBindingOb
                     ),
                   ),
               ],
-            ],
           ),
         ),
       ),
